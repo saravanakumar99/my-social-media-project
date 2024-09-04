46c34df1c540133408b4d2c644837e481fa91edb
saravanaakumar99 <saravanakumar.vcb@gmail.com>
2024-09-04 09:42:56 +0530

    firebase changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,23 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth,GoogleAuthProvider} from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBiO4r7JX1bjpSXFn-FqhtkWsiAgbfsMo4",
-  authDomain: "my-social-media-app-5950a.firebaseapp.com",
-  projectId: "my-social-media-app-5950a",
-  storageBucket: "my-social-media-app-5950a.appspot.com",
-  messagingSenderId: "831880551886",
-  appId: "1:831880551886:web:d8c62a2d08d7803c5f1f9f"
+  apiKey: "AIzaSyCHWkbDd6d92NlKlfaNKpElBejBxhoI1d0",
+  authDomain: "duo-quotes-2f6c8.firebaseapp.com",
+  projectId: "duo-quotes-2f6c8",
+  storageBucket: "duo-quotes-2f6c8.appspot.com",
+  messagingSenderId: "1044178215397",
+  appId: "1:1044178215397:web:47468368c9888dab144f0c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
